8200388a37b9ff0df2831962a8a568ebf2c5a5b9
Bentura Peralez <bperalez001@gmail.com>
2021-10-30 07:25:56 -0500

    revisions

---
+++ readme.js
@@ -0,0 +1,99 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is your title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your decription of the project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'what is your table of contents?',
+        name: 'contents',
+    },
+    {
+        type: 'input',
+        message: 'What are your installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What are your usage instructions?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are your contribution guidelines',
+        name: 'contribution',
+    },
+
+    {
+        type: 'input',
+        message: 'What are your test instructions?',
+        name: 'test',
+    },
+
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['ACADEMIC Free License v3.0', 'Creative Comons License Family', 'ISC', 'MIT', 'Apache License 2.0',],
+        name: 'license'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter your gitHub link here',
+        name: 'gitHub',
+    },
+
+    {
+        type: 'input',
+        message: 'Enter your email here?',
+        name: 'email',
+    },
+
+    
+]).then (function (surveyResults){
+    console.log(surveyResults)
+    var readme=`
+    #Title: ${surveyResults.title}
+
+    license: ${surveyResults.license}
+
+    ##Description: 
+${surveyResults.description}
+
+    ##Contents:
+${surveyResults.contents}
+  
+
+    ##Installation instructions:
+${surveyResults.installation}
+
+    ##Usage instructions:
+${surveyResults.usage}
+
+    ##Contribution guidelines:
+${surveyResults.contribution}
+
+    ##Test instructions:
+${surveyResults.test}
+
+    #Questions
+GitHub: ${surveyResults.gitHub}
+
+Email: ${surveyResults.email}
+
+`
+  
+    fs.writeFile('readme.md',readme,function(err){
+    if (err) {
+        console.log(err)
+    };
+    })
+    })
---
+++ readme.md
@@ -0,0 +1,29 @@
+
+    #Title: amazingProject
+
+    license: MIT
+
+    ##Description: 
+super amazing project on things related to computer science such as ....
+
+    ##Contents:
+1.installation 2.how to use
+  
+
+    ##Installation instructions:
+Make sure to copy and paste the github into your terminal
+
+    ##Usage instructions:
+do not use this to make a profit off of it
+
+    ##Contribution guidelines:
+I will be the one to approve whether or not a contribution is valid
+
+    ##Test instructions:
+N/A
+
+    #Questions
+GitHub: https://github.com/nicolewallace09/team-profile-generator/blob/master/index.js
+
+Email: noemail@gmail.com
+
